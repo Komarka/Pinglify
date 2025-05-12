7a775a68cac13a636de91adfd0b76b7f1d35d25e
Illia Komar <illia.komar@beehero.io>
2025-05-12 11:49:06 +0200

    fixed cors issue

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -5,9 +5,13 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  const allowedOrigins = [
+    'http://localhost:5173',
+    'https://pinglify.netlify.app',
+  ];
+
   app.enableCors({
-    origin: '*',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    origin: allowedOrigins,
     credentials: true,
   });
 

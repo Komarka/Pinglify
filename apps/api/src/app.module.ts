5fa5f030c9df1a6dab21f870761be32c9e8b046d
Illia Komar <illia.komar@beehero.io>
2025-05-12 14:18:21 +0200

    ping endpoint is added

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -5,6 +5,7 @@ import { ConfigModule } from '@nestjs/config';
 import { BotModule } from './bot/bot.module';
 import { UserModule } from './user/user.module';
 import { BotService } from './bot/bot.service';
+import { AppController } from './app.controller';
 
 @Module({
   imports: [
@@ -14,6 +15,7 @@ import { BotService } from './bot/bot.service';
     BotModule,
     UserModule,
   ],
+  controllers: [AppController],
   providers: [BotService],
 })
 export class AppModule {}
